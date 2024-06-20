e5a90996a953e1cf009737c3ff15010f9262f5c1
mamieorine <mamieorine@gmail.com>
2024-06-20 10:55:18 +0100

    initial commit

---
+++ bin/cdk-pipeline.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const cdk = require('aws-cdk-lib');
+const { CdkPipelineStack } = require('../lib/cdk-pipeline-stack');
+
+const app = new cdk.App();
+new CdkPipelineStack(app, 'CdkPipelineStack', {
+  env: {
+    account: '975050056288',
+    region: 'eu-west-2',
+  }
+});
---
+++ test/cdk-pipeline.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const CdkPipeline = require('../lib/cdk-pipeline-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-pipeline-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CdkPipeline.CdkPipelineStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
