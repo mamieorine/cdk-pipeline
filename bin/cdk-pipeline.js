2616cb633f82f1a9c1466463977044b420ee4d39
mamieorine <mamieorine@gmail.com>
2024-06-26 16:07:39 +0100

    chore: add connection

--- bin/cdk-pipeline.js
+++ bin/cdk-pipeline.js
@@ -4,4 +4,12 @@ const cdk = require('aws-cdk-lib');
 const { CdkPipelineStack } = require('../lib/cdk-pipeline-stack');
 
 const app = new cdk.App();
-new CdkPipelineStack(app, 'CdkPipelineStack', {});
+new CdkPipelineStack(app, 'CdkPipelineStack', {
+    env: {
+        account: '975050056288',
+        region: 'eu-west-2',
+    }
+});
+
+app.synth();
+
